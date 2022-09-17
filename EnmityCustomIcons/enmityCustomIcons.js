d05579970a6f5204f2a2861620d067817b767c1a
DeltAndy <deltandy123@gmail.com>
2022-09-16 23:12:33 -0400

    Fix versions

--- EnmityCustomIcons/enmityCustomIcons.js
+++ EnmityCustomIcons/enmityCustomIcons.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const fs = require("fs");
+const unzipper = require("unzipper");
 
 /** Steps:
  * 1. Get the latest release from https://api.github.com/repos/devicarus/enmity-custom-icons/releases/latest
@@ -21,20 +22,30 @@ axios
     const dest = fs.createWriteStream("./PlumpyCord.ipa");
     plumpycord.data.pipe(dest);
 
-    var enmity = await axios.get(json.data.assets[1].browser_download_url, {
-      responseType: "stream",
-    });
-
     // TODO: Add Debrandcord
 
     // Step 3
     const source = fs.readFileSync("./plumpycord.json", "utf8");
     const sourceJson = JSON.parse(source);
 
-    sourceJson.apps[0].version = json.data.tag_name.replace("v", "");
-    sourceJson.apps[0].versionDate = json.data.published_at;
-    sourceJson.apps[0].versionDescription = json.data.body;
-    sourceJson.apps[0].size = json.data.assets[0].size;
+    // Unzip the IPA and find the version in the Info.plist
+    plumpycord.data
+      .pipe(unzipper.ParseOne(/Discord\.app\/Info\.plist/))
+      .on("entry", (entry) => {
+        entry.pipe(fs.createWriteStream("./Info.plist"));
 
-    fs.writeFileSync("./plumpycord.json", JSON.stringify(sourceJson, null, 2));
+        entry.on("end", () => {
+          const infoPlist = fs.readFileSync("./Info.plist", "utf8");
+          const version = infoPlist.match(/<key>CFBundleShortVersionString<\/key>\s*<string>(.*?)<\/string>/)[1];
+
+          sourceJson.apps[0].version = version;
+          sourceJson.apps[0].versionDate = json.data.published_at;
+          sourceJson.apps[0].versionDescription = json.data.body;
+          sourceJson.apps[0].size = json.data.assets[0].size;
+      
+          fs.writeFileSync("./plumpycord.json", JSON.stringify(sourceJson, null, 2));
+        });
+        
+      })
+
   });
