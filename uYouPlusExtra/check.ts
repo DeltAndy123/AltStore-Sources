12225eb81fe20a7a0c1491c3539035aae01eb756
DeltAndy123 <deltandy123@gmail.com>
2023-01-09 18:14:06 -0500

    Test actions

--- uYouPlusExtra/check.ts
+++ uYouPlusExtra/check.ts
@@ -7,7 +7,8 @@ const res = await axios.get('https://api.github.com/repos/qnblackcat/uYouPlus/re
 const remoteVer = res.data.tag_name.match(/v(.*?)-/)[1]
 
 const sourceJSON: Source = JSON.parse(fs.readFileSync('./uyouplusextra.json').toString())
-const currentVer = sourceJSON.apps[0].versions[0].version
+const current = sourceJSON.apps[0].versions[0]
+const currentVer = current ? current.version : "0.0.0"
 
 process.stdout.write(JSON.stringify({
   remote: remoteVer,
