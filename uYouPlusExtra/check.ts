5e674a52352a431b5bca72f808c8b9b419a4de38
DeltAndy <105518328+DeltAndy123@users.noreply.github.com>
2023-01-18 21:01:55 -0500

    bruh

--- uYouPlusExtra/check.ts
+++ uYouPlusExtra/check.ts
@@ -3,7 +3,7 @@ import fs from 'fs'
 
 (async ()=>{
 
-const res = await axios.get('https://api.github.com/repos/qnblackcat/uYouPlus/releases/latest')
+const res = await axios.get('https://api.github.com/repos/arichorn/uYouPlusExtra/releases/latest')
 const remoteVer = res.data.tag_name.match(/v(.*?)-/)[1]
 
 const sourceJSON: Source = JSON.parse(fs.readFileSync('./uyouplusextra.json').toString())
